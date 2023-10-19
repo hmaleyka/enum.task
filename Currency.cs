ead288014f648b6c612cbe7cc28e00f864493154
Maleyka heybatova <maleykaheybatova1011@gmail.com>
2023-10-19 08:09:06 +0400

    the enumtask was added

---
+++ Currency.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hometask_
+{
+    internal enum Currency
+    {
+        Usd ,
+        Euro ,
+        Try ,
+        Rub ,
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace hometask_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            foreach (Currency currencies in Enum.GetValues(typeof(Currency)))
+            {
+                Console.WriteLine(currencies);
+            }
+
+
+            Console.WriteLine("enter your AZE value for exchanging with other currency values");
+            double amount1 = Convert.ToDouble(Console.ReadLine());
+
+            Currency currencyType1 = Currency.Usd;
+
+            Currency currencyType2 = Currency.Euro;
+
+            Currency currencyType3 = Currency.Try;
+
+            Currency currencyType4 = Currency.Rub;
+
+            FindCurrency(currencyType1, amount1);
+            FindCurrency(currencyType2, amount1);
+            FindCurrency(currencyType3, amount1);
+            FindCurrency(currencyType4, amount1);
+
+
+             static void FindCurrency(Currency currency, double aznValue)
+            {
+                switch (currency)
+                {
+                    case Currency.Usd:
+                        Console.WriteLine("the amount with dollar  " + aznValue * 0.588256 +  " $"); // Dollar
+                        break;
+                    case Currency.Euro:
+                        Console.WriteLine( "the amount with euro  " +  aznValue * 0.554554 + " euro"); 
+                        break;
+                    case Currency.Try:
+                         Console.WriteLine( "the amount with turkish lira  " + aznValue * 16.4622 + " turkish lira"); 
+                        break;
+                    case Currency.Rub:
+                        Console.WriteLine("the amount with russian ruble  " + aznValue * 57.6539 + " russian ruble"); 
+                        break;
+                    default:
+                        Console.WriteLine( "Invalid currency");
+                        break;
+
+                }
+            }
+        }
+    }
+}
